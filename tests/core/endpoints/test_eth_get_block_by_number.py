4f583d5a3f7fd6c0cadb7b9662b6df46302126be
voith <voithjm1@gmail.com>
2018-10-11 03:08:46 +0530

    add test for eth_getBlockByNumber with full_transactions=True

--- tests/core/endpoints/test_eth_get_block_by_number.py
+++ tests/core/endpoints/test_eth_get_block_by_number.py
@@ -5,3 +5,25 @@ def test_eth_getBlock(rpc_client):
         params=['0x01']
     )
     assert isinstance(block, dict)
+
+
+def test_eth_getBlock_full_tx(rpc_client, accounts):
+    rpc_client(
+        method="eth_sendTransaction",
+        params=[{
+            "from": accounts[0],
+            "to": accounts[1],
+            "value": hex(1234),
+            "data": "0x1234",
+            "gas": hex(100000),
+            "gasPrice": hex(4321),
+        }],
+    )
+    block_number = rpc_client('eth_blockNumber')
+    block = rpc_client(
+        'eth_getBlockByNumber',
+        params=[hex(block_number), True]
+    )
+    assert len(block['transactions']) == 1
+    assert 'gasPrice' in block['transactions'][0]
+    assert isinstance(block, dict)
