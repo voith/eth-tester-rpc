43604284503df4583db2fe92545cdc26eb7fe22c
voith <voithjm1@gmail.com>
2019-06-05 19:19:30 +0530

    add test for signing with new privatekey

--- tests/core/endpoints/test_eth_sign.py
+++ tests/core/endpoints/test_eth_sign.py
@@ -1,3 +1,5 @@
+from eth_account import Account
+
 from tests.utils import (
     ecrecover,
 )
@@ -8,3 +10,12 @@ def test_eth_sign(rpc_client):
     message = "0xdeadbeaf"
     signature = rpc_client('eth_sign', params=[account, message])
     assert ecrecover(signature, message) == account
+
+
+def test_eth_sign_new_private_key(rpc_client):
+    account = Account.create()
+    result = rpc_client('personal_importRawKey', params=[account.privateKey.hex()])
+    assert result == account.address
+    message = "0xdeadbeaf"
+    signature = rpc_client('eth_sign', params=[account.address, message])
+    assert ecrecover(signature, message) == account.address
